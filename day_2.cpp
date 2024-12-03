9b0fc785beca0967b62f41cc5f103127e4002419
Olivier Kessler <olivier.kessler@protonmail.com>
2024-12-03 13:16:41 +0100

    Finish day 2

--- day_2.cpp
+++ day_2.cpp
@@ -1,22 +1,20 @@
-
-#include <algorithm>
-#include <array>
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
-
-int main(int argc, char **argv) {
-    
-    FILE * fp;
-    char * line = NULL;
+int main(int argc, char** argv)
+{
+    FILE* fp;
+    char* line = NULL;
     size_t len = 0;
     ssize_t read;
     int answer = 0;
-    std::string buf; 
-    int last;
-    int current;
-    int non_decreasing;
+    std::string buf;
+    bool non_decreasing;
+    std::vector<int> report;
+    int p1, p2, current, last;
+    bool valid = true;
 
     fp = fopen("day_2.data", "r");
     if (fp == NULL)
@@ -25,32 +23,55 @@ int main(int argc, char **argv) {
     while ((read = getline(&line, &len, fp)) != -1) {
         printf("Retrieved line of length %zu:\n", read);
         printf("%s", line);
-        last = -1;
-        current = -1;
-        non_decreasing = 0;
-        
-        for(int index = 0; index < read;index++){
-            if (line[index] == ' '){
-                current = std::stoi(buf);
-                if (last > -1){
-                    
-                }
-                last = current;
+        buf = "";
+
+        report = {};
+
+        for (int index = 0; index < read; index++) {
+            if (line[index] == ' ' || line[index] == '\n') {
+                report.push_back(std::stoi(buf));
+                buf = "";
             } else {
-                buf+=line[index];
+                buf += line[index];
             }
         }
 
+        p1 = 0;
+        p2 = 1;
 
+        if (report[1] >= report[0]) {
+            non_decreasing = true;
+        } else {
+            non_decreasing = false;
+        }
+
+        valid = true;
+        while (p2 < report.size() && valid == true) {
+            current = report[p2];
+            last = report[p1];
+            if (non_decreasing == true) {
+                if (current <= last or current > last + 3) {
+                    valid = false;
+                }
+            } else {
+                if (current >= last or current < last - 3) {
+                    valid = false;
+                }
+            }
+
+            p1++;
+            p2++;
+        }
+
+        if (p2 == report.size()) {
+            answer++;
+        }
     }
 
     fclose(fp);
     if (line)
         free(line);
-    exit(EXIT_SUCCESS);   
-    
-
-
 
     std::cout << answer << std::endl;
+    exit(EXIT_SUCCESS);
 }
