fcbc6265c9b12c633ae993457e905327fdff675f
Olivier Kessler <olivier.kessler@protonmail.com>
2024-12-05 18:47:14 +0100

    Add some more

--- day_4.cpp
+++ day_4.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "utils.h"
 
-int find_xmas(std::vector<std::vector<char>> matrix, int i, int j){
+int find_xmas(std::vector<std::vector<char>> &matrix, int i, int j){
     std::vector<std::vector<int>> offsets = {
         {0,1},
         {1,0},
@@ -17,32 +17,46 @@ int find_xmas(std::vector<std::vector<char>> matrix, int i, int j){
     };
     int answer = 0;
     int i2,j2;
+    char elem;
 
     for(auto offset:offsets){
         i2 = i;
         j2 = j;
 
-        for(int step = 0; step< 4;i++){
-            if(step == 3){
-                if(matrix[i2][j2] == 'S'){
-                    answer+=1;
-                }
-            }else{
+        for(int step = 0; step < 4;step++){
+            elem = matrix[i2][j2];
 
+            if(step == 3 && elem == 'S'){
+                answer+=1;
+            } 
+            else if (step == 0 and elem == 'X'){
+                continue; 
             }
+            else if (step == 1 and elem == 'M') {
+                continue;
+            }
+            else if (step == 2 and elem == 'A') {
+                continue;
+            }
+            else {
+                break;
+            }
+            i2+=offset[0];
+            j2+=offset[1];
         }
     }
+
+    return answer;
 }
 
-int solve(std::vector<std::vector<char>> matrix)
+int solve(std::vector<std::vector<char>> &matrix)
 {
     int answer = 0;
+    std::cout << "Size matrix : " << matrix.size() << " * " << matrix[0].size() << std::endl;
 
     for(int i = 0 ; i< matrix.size();i++){
         for(int j = 0; j<matrix[0].size();j++){
-            if(matrix[i][j] == 'X'){
-                answer+=find_xmas(matrix,i,j);
-            }
+            answer+=find_xmas(matrix,i,j);
         }
     }    
 
@@ -79,10 +93,9 @@ int main(int argc, char** argv)
     if (line)
         free(line);
 
-    for(auto line: matrix){
-        print_vector(line);
-    }
-    
+    //for(auto line: matrix){
+     //   print_vector(line);
+    //}
 
     std::cout << solve(matrix) << std::endl;
 
