4bf288bba3ef830bb1b3b00276af1ca08055f30e
Olivier Kessler <olivier.kessler@protonmail.com>
2024-12-07 17:02:52 +0100

    Finish day 5

--- day_5.py
+++ day_5.py
@@ -9,8 +9,8 @@ def day_5_first():
             if "|" in line:
                 line_s = line.strip().split("|")
                 if line_s[1] not in need:
-                    need[line_s[1]] = []
-                need[line_s[1]].append(line_s[0])
+                    need[line_s[1]] = set()
+                need[line_s[1]].add(line_s[0])
             elif line.strip() == "":
                 continue
             else:
@@ -22,20 +22,16 @@ def day_5_first():
     for update in updates:
         valid = True
         seen = set() 
+        update_s = set(update)
 
         for page in update:
-            if valid is False:
-                break
-
             if page in seen:
                 continue
 
             if page in need: 
-                for dependency in need[page]:
-                    if dependency in update and dependency not in seen:
-                        print(f"{page} {dependency} {seen}")
-                        valid = False
-                        break
+                if len(need[page].intersection(update_s).difference(seen)) > 0:
+                    valid = False
+                    break
         
             seen.add(page)
 
@@ -45,8 +41,64 @@ def day_5_first():
     return ans
 
 def day_5_second():
+    need = {}
+    updates = []
+    incorrect_updates = []
 
-    return sum(mul[0] * mul[1] for mul in muls)
+    with open("day_5.data", "r") as file:
+        for line in file:
+            if "|" in line:
+                line_s = line.strip().split("|")
+                if line_s[1] not in need:
+                    need[line_s[1]] = set()
+                need[line_s[1]].add(line_s[0])
+            elif line.strip() == "":
+                continue
+            else:
+                updates.append(line.strip().split(","))
+     
+    
+    ans = 0
+
+    for update in updates:
+        valid = True
+        seen = set() 
+        update_s = set(update)
+
+
+        for page in update:
+            if page in seen:
+                continue
+
+            if page in need: 
+                if len(need[page].intersection(update_s).difference(seen)) > 0:
+                    valid = False
+                    break
+        
+            seen.add(page)
+
+        if not valid:
+            incorrect_updates.append(update)
+
+    
+    for update in incorrect_updates:
+        queue = update
+        sorted = []
+        seen = set()
+        update_s = set(update)
+
+        while len(queue) > 0:
+            page = queue.pop(0)
+            if len(need[page].intersection(update_s).difference(seen)) != 0:
+                queue.append(page)
+            else:
+                sorted.append(page)
+                seen.add(page)
+
+
+        ans+=int(sorted[len(sorted)//2])
+    
+    return ans
 
 
 if __name__ == "__main__":
