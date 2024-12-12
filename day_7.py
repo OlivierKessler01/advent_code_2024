c8379a8270f927589df39ef12eae1175ec5a7dee
Olivier Kessler <olivier.kessler@protonmail.com>
2024-12-12 08:13:15 +0100

    Add day 7 second part

--- day_7.py
+++ day_7.py
@@ -32,31 +32,35 @@ def day_7_first():
     return ans
 
 
-    
-
-
-
 def day_7_second():
-    ans = 0 
-    grid = []
-    direction =0 
-    curr = [0,0]
-    visited = set()
+    ans = 0
+
+    def rec(operands, index):
+        if index == 0:
+            return [int(operands[0])]
+        
+        next = rec(operands, index-1)
+        
+        new_next = []
+        for nn in next:
+            new_next.append(nn*int(operands[index]))
+            new_next.append(nn+int(operands[index]))
+            new_next.append(int(str(nn)+operands[index]))
+        
+        return new_next
 
     with open("day_7.data", "r") as file:
         for line in file:
-            line = list(line.strip())
-            grid.append(line)
-            
-            for nn, char in enumerate(line):
-                if char == "^":
-                    curr = [len(grid) -1, nn]
-                    break
+            line = line.strip()
+            split = line.split(":")
+            test_value = int(split[0])
+            operands = split[1]
+            operands = tuple(operands.split(" ")[1:])
+            results = rec(operands, len(operands)-1)
+            if test_value in results:
+                  ans+=test_value
 
-    m = len(grid)
-    n = len(grid[0])
-
-
+    return ans
 
 
 
